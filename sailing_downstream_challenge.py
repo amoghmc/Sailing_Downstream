4b9b44d8b14912c3e507a067e82fc4089be26cf6
amoghmc <amoghmc@github.com>
2024-01-30 06:30:54 +0530

    decompose input function and test it

--- sailing_downstream_challenge.py
+++ sailing_downstream_challenge.py
@@ -1,41 +1,51 @@
 """
 Write a Javascript, Python, or OCaml program that:
--accepts a list of integers
--emits an error message if the list is not a multiple of 10 in length
--returns or prints a list of integers based on the input list,
-  but with items at positions which are a multiple of 2 or 3 removed
+- accepts a list of integers
+- emits an error message if the list is not a multiple of 10 in length
+- returns or prints a list of integers based on the input list,
+but with items at positions which are a multiple of 2 or 3 removed
 """
 
 
 def main():
-    print("Enter a list of integers separated by space")
-    inp = input()
-    inp = list(inp.split())
+	inp = get_input()
+	if not inp:
+		print("Error: Invalid Input!")
+		return 1
 
-    if not check_multiple_of_10(inp):
-        print("Error: List is not a multiple of 10")
-        return 1
+	if not check_multiple_of_10(inp):
+		print("Error: List is not a multiple of 10")
+		return 2
 
-    inp = list(map(int, inp))
-    print(remove_multiples(inp))
+	print(remove_multiples(inp))
+
+
+def get_input():
+	inp = input("Enter a list of integers separated by space\n")
+	try:
+		inp = list(inp.split())
+		inp = list(map(int, inp))
+	except:
+		return None
+	return inp
 
 
 def check_multiple_of_10(inp: list):
-    if len(inp) % 10 != 0:
-        return False
-    return True
+	if len(inp) % 10 != 0:
+		return False
+	return True
 
 
 def remove_multiples(inp: list):
-    result = []
-    for i in range(len(inp)):
-        # skip positions that are multiples of 2 or 3
-        offset_i = i + 1
-        if offset_i % 2 == 0 or offset_i % 3 == 0:
-            continue
-        result.append(inp[i])
-    return result
+	result = []
+	for i in range(len(inp)):
+		# skip positions that are multiples of 2 or 3
+		offset_i = i + 1
+		if offset_i % 2 == 0 or offset_i % 3 == 0:
+			continue
+		result.append(inp[i])
+	return result
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- test.py
+++ test.py
@@ -1,30 +1,67 @@
 import unittest
-from sailing_downstream_challenge import check_multiple_of_10, remove_multiples
+from unittest.mock import patch
+from io import StringIO
+from sailing_downstream_challenge import (check_multiple_of_10,
+										  remove_multiples,
+										  get_input)
+
 
 class Unit_Test_Sailing_Downstream(unittest.TestCase):
 
-    def test_check_multiple_of_10(self):
-        result = check_multiple_of_10([])
-        self.assertEqual(result, True)
-        result = check_multiple_of_10([1, 2, 3])
-        self.assertEqual(result, False)
-        result = check_multiple_of_10([i for i in range(10)])
-        self.assertEqual(result, True)
-        result = check_multiple_of_10([i for i in range(20)])
-        self.assertEqual(result, True)
-        result = check_multiple_of_10([i for i in range(21)])
-        self.assertEqual(result, False)
+	# test with invalid input of letters
+	@patch('builtins.input', return_value='a b c d')
+	@patch('sys.stdout', new_callable=StringIO)
+	def test_process_input_letters(self, mock_stdout, mock_input):
+		result = get_input()
 
-    def test_remove_multiples(self):
-        result = remove_multiples([1, 2, 3])
-        self.assertEqual(result, [1])
-        result = remove_multiples([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        self.assertEqual(result, [1, 5, 7])
-        result = remove_multiples([])
-        self.assertEqual(result, [])
-        result = remove_multiples([1])
-        self.assertEqual(result, [1])
+		# Assertions
+		mock_input.assert_called_once_with("Enter a list of integers separated by space\n")
+		self.assertEqual(result, None)
+
+	# test with invalid input of ints separated by commas
+	@patch('builtins.input', return_value='1, 2, 3, 4')
+	@patch('sys.stdout', new_callable=StringIO)
+	def test_process_input(self, mock_stdout, mock_input):
+		result = get_input()
+
+		# Assertions
+		mock_input.assert_called_once_with("Enter a list of integers separated by space\n")
+		self.assertEqual(result, None)
+
+	# test with valid input
+	@patch('builtins.input', return_value='1 2 3 4')
+	@patch('sys.stdout', new_callable=StringIO)
+	def test_process_input(self, mock_stdout, mock_input):
+		result = get_input()
+		printed_output = mock_stdout.getvalue().strip()
+
+		# Assertions
+		mock_input.assert_called_once_with("Enter a list of integers separated by space\n")
+		self.assertEqual(result, [1, 2, 3, 4])
+		self.assertEqual(printed_output, '')
+
+	def test_check_multiple_of_10(self):
+		result = check_multiple_of_10([])
+		self.assertEqual(result, True)
+		result = check_multiple_of_10([1, 2, 3])
+		self.assertEqual(result, False)
+		result = check_multiple_of_10([i for i in range(10)])
+		self.assertEqual(result, True)
+		result = check_multiple_of_10([i for i in range(20)])
+		self.assertEqual(result, True)
+		result = check_multiple_of_10([i for i in range(21)])
+		self.assertEqual(result, False)
+
+	def test_remove_multiples(self):
+		result = remove_multiples([1, 2, 3])
+		self.assertEqual(result, [1])
+		result = remove_multiples([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+		self.assertEqual(result, [1, 5, 7])
+		result = remove_multiples([])
+		self.assertEqual(result, [])
+		result = remove_multiples([1])
+		self.assertEqual(result, [1])
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
